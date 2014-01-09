d874062ca98f22944ebe8ff3af6a37c39bf540c5
Benjamin Mann <bjmn55@gmail.com>
2014-01-09 05:43:33 -0800

    Moved some use flags in base/desktop package.use and added to kde bootstrap.sh
    Moved some use flags specific to KDE4 to the KDE base profile
    Added a few more use flags to negate initially in the KDE profiles bootstrap.sh so things may start into the world build quicker

--- profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
+++ profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Emerge packages with use flags set to stop circular dependencies
-USE="-postgres -java -gtk" emerge -1 dev-libs/cyrus-sasl net-dns/avahi
+USE="-postgres -java -gtk -gtk3 -qt4" emerge -1 dev-libs/cyrus-sasl net-dns/avahi
