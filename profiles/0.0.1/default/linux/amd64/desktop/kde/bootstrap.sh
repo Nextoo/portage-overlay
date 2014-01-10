d32957e85fb6348887888ba898b93ffc708ab42d
Benjamin Mann <bjmn55@gmail.com>
2014-01-09 21:37:46 -0800

    Removed the previously added, but unneeded, USE flags

--- profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
+++ profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Emerge packages with use flags set to stop circular dependencies
-USE="-postgres -java -gtk -gtk3 -qt4 -X -doc" emerge -1 dev-libs/cyrus-sasl net-dns/avahi
+USE="-postgres -java -gtk -gtk3 -qt4" emerge -1 dev-libs/cyrus-sasl net-dns/avahi
 emerge -1 --nodeps dev-java/icedtea-bin
