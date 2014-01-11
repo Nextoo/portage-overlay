3bc91667a8bc936c647bbc98b12b8645993a45e6
Benjamin Mann <bjmn55@gmail.com>
2014-01-11 07:31:34 -0800

    Added python-updater to KDE bootstrap.sh
    Attempting to update python before building world to see if that fixes the "dev-lang/python:3.2 is not installed" issue coming from sys-libs/libcap-ng

--- profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
+++ profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
@@ -2,3 +2,4 @@
 # Emerge packages with use flags set to stop circular dependencies
 USE="-postgres -java -gtk -gtk3 -qt4" emerge -1Nu dev-libs/cyrus-sasl net-dns/avahi
 emerge -1Nu --nodeps dev-java/icedtea-bin
+python-updater
