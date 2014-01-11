83ed81b2a56295bd6d5774d55494037b627b1e6f
Benjamin Mann <bjmn55@gmail.com>
2014-01-11 15:47:13 -0800

    Changed 'python-updater' for just installing python:3.2 in KDE bootstrap

--- profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
+++ profiles/0.0.1/default/linux/amd64/desktop/kde/bootstrap.sh
@@ -2,4 +2,4 @@
 # Emerge packages with use flags set to stop circular dependencies
 USE="-postgres -java -gtk -gtk3 -qt4" emerge -1Nu dev-libs/cyrus-sasl net-dns/avahi
 emerge -1Nu --nodeps dev-java/icedtea-bin
-python-updater
+emerge -1Nu dev-lang/python:3.2
