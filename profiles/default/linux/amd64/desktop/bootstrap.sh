d49b0a3906dd62ca1bc76635951bedbcbb74f795
Aaron Ten Clay <aarontc@aarontc.com>
2014-04-18 16:07:50 -0700

    Fixing CUPS USE flags

--- profiles/default/linux/amd64/desktop/bootstrap.sh
+++ profiles/default/linux/amd64/desktop/bootstrap.sh
@@ -2,4 +2,4 @@
 # Emerge packages with use flags set to stop circular dependencies
 USE="-opengl" emerge -1Nu app-emulation/emul-linux-x86-xlibs
 USE="-gtk" emerge -1Nu net-dns/avahi
-
+USE="-java" emerge -1Nu net-print/cups
