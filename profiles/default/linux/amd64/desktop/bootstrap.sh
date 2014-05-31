6e498a99aa945c1fe4ddbbb49d669409245e2f31
Aaron Ten Clay <aarontc@aarontc.com>
2014-05-31 11:42:14 -0700

    Update bootstrap.sh

--- profiles/default/linux/amd64/desktop/bootstrap.sh
+++ profiles/default/linux/amd64/desktop/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Emerge packages with use flags set to stop circular dependencies
+# Merge packages with use flags set to stop circular dependencies
 USE="-opengl" emerge -1Nu app-emulation/emul-linux-x86-xlibs
 USE="-gtk -java" emerge -1Nu net-dns/avahi net-print/cups
